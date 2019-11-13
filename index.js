0f3b86f3cf2445fc516ce43810358152906a9774
pradnyajadhav <pcjadhav6767@gmail.com>
2019-11-13 15:22:30 +0530

    first change in index

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var app =  express();
 const port=4000;
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 //Use Middleware
 app.use(express.json());
